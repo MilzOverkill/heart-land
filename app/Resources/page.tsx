30e32b7e4f444f569cfb7fde282c40c6adf8886d
knightowlrehan <rehan@knightowl.online>
2026-01-28 17:16:35 +0530

    feat : completed  the school & professional tie section

--- app/Resources/page.tsx
+++ app/Resources/page.tsx
@@ -3,7 +3,7 @@ import SuccessStory from '@/components/successStory';
 import LatestNews from '@/components/latestNews';
 import BlogCards from '@/components/blogCards';
 import CommunityPillar from '@/components/CommunityPillar';
-
+import SchoolProfessionalities from '@/components/SchoolProfessionalities';
 
 export default function Resources() {
   return (
@@ -13,6 +13,7 @@ export default function Resources() {
     {/* <LatestNews />
     <BlogCards /> */}
     <CommunityPillar/>
+    <SchoolProfessionalities/>
     </ >
 
     
